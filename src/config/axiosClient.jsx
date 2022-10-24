fbecf72e487720fc116eccb9ee2a84d1e9879e7d
david9go <websajang@gmail.com>
2022-10-24 17:15:03 +0200

    third commit

--- src/config/axiosClient.jsx
+++ src/config/axiosClient.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 /** useing axiosClient or any other variable name, instead of axios, will make that calling axiosClient.post() for example set the beggining of the url with VITE_BACKEND_ULR/api **/
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}api`
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
 })
 
 export default axiosClient
